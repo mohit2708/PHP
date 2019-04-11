8c962ec725bf1a9a4c241288c6a6fc3f347b01ba
Mohit Saxena <42208944+mohit2708@users.noreply.github.com>
2019-04-11 10:13:49 +0530

    Update program.php

--- Program/program.php
+++ Program/program.php
@@ -1,1 +1,29 @@
-dfg
+Swap Number:- 
+<?php
+  function abc($a,$b)		
+  {
+  echo "Value of a: $a</br>";
+  echo "Value of b: $b</br>";
+  $temp=$a;
+  $a=$b;
+  $b=$temp;
+  echo "Value of a: $a</br>";
+  echo "Value of b: $b</br>";
+  }
+  $n1 = 4;
+  $n2 = 2;
+  abc ($n1,$n2);
+?>
+
+Swap Number Without Third Variable:- 
+<?php
+$a = 5;
+$b = 9;
+
+$a =  $a + $b;  // 5 + 6 = 11
+$b = $a - $b;   // 11 - 6 = 5
+$a = $a - $b;  // 11 - 5 = 6
+
+echo $a . ',' . $b;
+?>
+
